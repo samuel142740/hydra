5ac0c04c69d972ca25b50ce63664a9b06c1fc35f
Zamitto <167933696+zamitto@users.noreply.github.com>
2025-01-05 22:03:20 -0300

    fix: error handling

--- src/main/services/hydra-api.ts
+++ src/main/services/hydra-api.ts
@@ -154,7 +154,8 @@ export class HydraApi {
         (error) => {
           logger.error(" ---- RESPONSE ERROR -----");
           const { config } = error;
-          const data = JSON.parse(config.data);
+
+          const data = JSON.parse(config.data ?? null);
 
           logger.error(
             config.method,
@@ -175,14 +176,22 @@ export class HydraApi {
               error.response.status,
               error.response.data
             );
-          } else if (error.request) {
-            const errorData = error.toJSON();
-            logger.error("Request error:", errorData.message);
-          } else {
-            logger.error("Error", error.message);
+
+            return Promise.reject(error as Error);
           }
-          logger.error(" ----- END RESPONSE ERROR -------");
-          return Promise.reject(error);
+
+          if (error.request) {
+            const errorData = error.toJSON();
+            logger.error("Request error:", errorData.code, errorData.message);
+            return Promise.reject(
+              new Error(
+                `Request failed with ${errorData.code} ${errorData.message}`
+              )
+            );
+          }
+
+          logger.error("Error", error.message);
+          return Promise.reject(error as Error);
         }
       );
     }
@@ -262,7 +271,7 @@ export class HydraApi {
     };
   }
 
-  private static handleUnauthorizedError = (err) => {
+  private static readonly handleUnauthorizedError = (err) => {
     if (err instanceof AxiosError && err.response?.status === 401) {
       logger.error(
         "401 - Current credentials:",
